aa465e68075cc35f5d660005a910266cea1a8262
Huseyin Eryilmaz <dev.huseyineryilmaz@gmail.com>
2022-11-15 08:41:44 +0300

    create basic template

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import data from '../data/data';
 
 class Page extends React.Component {
     constructor(props) {
         super(props);
+        this.state= {
+            index: Math.floor(Math.random() * 8)
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState ({
+            index: Math.floor(Math.random() * 7)
+        })
     }
 
     render() {
+        let quote = data[this.state.index];
         return (
-            <div></div>
+            <div id='quote-box' className='col'>
+                <div id='text' className='row'>{quote.quote}</div>
+                <div id='author' className='row'>{quote.person}</div>
+                <a id='tweet-quote' href="https://twitter.com/intent/tweet"></a>
+                <button id='new-quote' onClick={this.handleClick}>New quote</button>
+            </div>
         )
     }
 }
