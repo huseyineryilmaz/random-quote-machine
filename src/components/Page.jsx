d367528a3e6f21247c922d9c7c1816c9043e4522
Huseyin Eryilmaz <dev.huseyineryilmaz@gmail.com>
2022-11-17 23:56:33 +0300

    set background random color

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -7,27 +7,31 @@ class Page extends React.Component {
     constructor(props) {
         super(props);
         this.state= {
-            index: Math.floor(Math.random() * 8)
+            index: Math.floor(Math.random() * 7),
+            bgColor: 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
         this.setState ({
-            index: Math.floor(Math.random() * 7)
+            index: Math.floor(Math.random() * 7),
+            bgColor: 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')'
         })
     }
 
     render() {
         let quote = data[this.state.index];
         return (
-            <div id='quote-box' className='col'>
-                <div id='text' className='row'>{quote.quote}</div>
-                <div id='author' className='row'>{quote.person}</div>
-                <a id='tweet-quote' href="https://twitter.com/intent/tweet">
-                    <FontAwesomeIcon icon={faTwitter} />
-                </a>
-                <button id='new-quote' onClick={this.handleClick}>New quote</button>
+            <div id='page-body' style={{backgroundColor: this.state.bgColor}}>
+                <div id='quote-box' className='col'>
+                    <div id='text' className='row'>{quote.quote}</div>
+                    <div id='author' className='row'>{quote.person}</div>
+                    <a id='tweet-quote' href="https://twitter.com/intent/tweet" style={{backgroundColor: this.state.bgColor}}>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                    <button id='new-quote' onClick={this.handleClick} style={{backgroundColor: this.state.bgColor}}>New quote</button>
+                </div>
             </div>
         )
     }
